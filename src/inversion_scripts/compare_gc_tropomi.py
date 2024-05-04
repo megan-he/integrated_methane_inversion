73ebcde5377a13f0161a776dbb66c281eb1039ed
Megan He <mhe@g.harvard.edu>
2024-05-04 18:13:35 -0400

    change to match viz notebook script

--- src/inversion_scripts/compare_gc_tropomi.py
+++ src/inversion_scripts/compare_gc_tropomi.py
@@ -8,106 +8,66 @@ import glob
 import re
 from utils import load_obj, plot_field
 
-def list_files(directory):
-    files = glob.glob(f"{directory}/*.pkl")
-    files.sort()
-    dt = []
-    for f in files:
-        match = re.search(r'CH4____(.*?)_', f)
-        date_time = match.group(1)
-        date_time = f"{date_time[:4]}-{date_time[4:6]}-{date_time[6:8]}T{date_time[9:11]}:{date_time[11:13]}:{date_time[13:]}"
-        dt.append(np.datetime64(date_time))
-    return dt
-
-def match_observations(prior_directory, posterior_directory):
-
-    # Load TROPOMI/GEOS-Chem and Jacobian matrix data from the .nc file
-    dat = xr.open_dataset(prior_directory)
-    dat.close()
-
-    # Extract the timesteps in a 1d array
-    timesteps = dat["geoschem_methane"].coords['time'].values
-
-    # Match to closest posterior timestep (TROPOMI retrieval time)
-    posterior_timesteps = list_files(posterior_directory)
-    diffs = np.abs(timesteps[:, np.newaxis] - np.array(posterior_timesteps))
-    timesteps_match = timesteps[np.argmin(diffs, axis=0)]
+# Get observed and GEOS-Chem-simulated TROPOMI columns
+def aggregate_data(data_dir, data_posterior):
     
-    time = np.array([])
+    files = np.sort(os.listdir(data_dir))
     lat = np.array([])
     lon = np.array([])
-    tropomi_gc_grid = np.array([])
-    tropomi_sat_grid = np.array([])
+    tropomi = np.array([])
     geos_prior = np.array([])
     geos_posterior = np.array([])
-    posterior_lat = np.array([])
-    posterior_lon = np.array([])
     observation_count = np.array([])
-    observation_count_posterior = np.array([])
-    
-    # Load GC/TROPOMI data, obs counts, lats, lons. TROPOMI data is mapped onto GC grid already
-    for t in range(len(timesteps_match)):
 
-        GC_all = dat["geoschem_methane"].values[t,:,:]
-        non_nans = np.where(~np.isnan(GC_all))
-        GC = GC_all[non_nans]
-        
-        tropomi_all = dat["tropomi_methane"].values[t,:,:]
-        tropomi_methane_gc_grid = tropomi_all[non_nans]
-
-        tropomi_lat = dat["lat_sat"].values[t,:,:]
-        tropomi_lat = tropomi_lat[non_nans]
-        tropomi_lon = dat["lon_sat"].values[t,:,:]
-        tropomi_lon = tropomi_lon[non_nans]
-        
-        obs_count_all = dat["observation_count"].values[t,:,:]
-        obs_count = obs_count_all[non_nans]
-
-        t_array = np.full_like(tropomi_methane_gc_grid, t)
-
-        # Record lat, lon, tropomi ch4, and geos ch4
-        time = np.concatenate((time, t_array))
-        lat = np.concatenate((lat, tropomi_lat))
-        lon = np.concatenate((lon, tropomi_lon))
-        tropomi_gc_grid = np.concatenate((tropomi_gc_grid, tropomi_methane_gc_grid))
-        geos_prior = np.concatenate((geos_prior, GC))
-        observation_count = np.concatenate((observation_count, obs_count))
-    
-    # Load posterior GC methane
-    post_files = np.sort(os.listdir(posterior_directory))
-    for f in post_files:
-        pth_posterior = os.path.join(posterior_directory, f)
-        obj_posterior = load_obj(pth_posterior)
-        obs_GC_posterior = obj_posterior["obs_GC"][:,1]
-        tropomi_methane_sat_grid = obj_posterior["obs_GC"][:,0]
-        lat_sat = obj_posterior["obs_GC"][:,3]
-        lon_sat = obj_posterior["obs_GC"][:,2]
-        obs_count_post = obj_posterior["obs_GC"][:,4]
-        tropomi_sat_grid = np.concatenate((tropomi_sat_grid, tropomi_methane_sat_grid))
-        geos_posterior = np.concatenate((geos_posterior, obs_GC_posterior))
-        posterior_lat = np.concatenate((posterior_lat, lat_sat))
-        posterior_lon = np.concatenate((posterior_lon, lon_sat))
-        observation_count_posterior = np.concatenate((observation_count_posterior, obs_count_post))
+    for f in files:
+        # Get paths
+        if ".pkl" in f:
+            pth = os.path.join(data_dir, f)
+            pth_posterior = os.path.join(data_posterior, f)
+            # Load TROPOMI/GEOS-Chem and Jacobian matrix data from the .pkl file
+            obj = load_obj(pth)
+            obj_posterior = load_obj(pth_posterior)
+            # If there aren't any TROPOMI observations on this day, skip
+            if obj["obs_GC"].shape[0] == 0:
+                continue
+            # Otherwise, grab the TROPOMI/GEOS-Chem data
+            obs_GC = obj["obs_GC"]
+            obs_GC_posterior = obj_posterior["obs_GC"]
+            # Only consider data within latitude and longitude bounds
+            ind = np.where(
+                (obs_GC[:, 2] >= lon_bounds[0])
+                & (obs_GC[:, 2] <= lon_bounds[1])
+                & (obs_GC[:, 3] >= lat_bounds[0])
+                & (obs_GC[:, 3] <= lat_bounds[1])
+            )
+            if len(ind[0]) == 0:  # Skip if no data in bounds
+                continue
+            if obs_GC.shape[0] != obs_GC_posterior.shape[0]:
+                continue
+            obs_GC = obs_GC[ind[0], :]  # TROPOMI and GEOS-Chem data within bounds
+            obs_GC_posterior = obs_GC_posterior[ind[0], :]
+            # Record lat, lon, tropomi ch4, and geos ch4
+            lat = np.concatenate((lat, obs_GC[:, 3]))
+            lon = np.concatenate((lon, obs_GC[:, 2]))
+            tropomi = np.concatenate((tropomi, obs_GC[:, 0]))
+            geos_prior = np.concatenate((geos_prior, obs_GC[:, 1]))
+            observation_count = np.concatenate((observation_count, obs_GC[:, 4]))
+            geos_posterior = np.concatenate((geos_posterior, obs_GC_posterior[:, 1]))
 
     df = pd.DataFrame()
-    df["time"] = time
     df["lat"] = lat
     df["lon"] = lon
-    df["tropomi"] = tropomi_gc_grid
+    df["tropomi"] = tropomi
     df["geos_prior"] = geos_prior
-    df["diff_tropomi_prior"] = geos_prior - tropomi_gc_grid
+    df["geos_posterior"] = geos_posterior
+    df["diff_tropomi_prior"] = geos_prior - tropomi
+    df["diff_tropomi_posterior"] = geos_posterior - tropomi
     df["observation_count"] = observation_count
 
-    df_post = pd.DataFrame()
-    df_post["geos_posterior"] = geos_posterior
-    df_post["diff_tropomi_posterior"] = geos_posterior - tropomi_sat_grid
-    df_post["lat"] = posterior_lat
-    df_post["lon"] = posterior_lon
-    df_post["observation_count"] = observation_count_posterior
+    return df
 
-    return df, df_post
 
-def plot_comparison(lat_bounds, lon_bounds):
+def plot_comparison(ds, lat_bounds, lon_bounds):
 
     # Open the state vector file
     state_vector_filepath = "../StateVector.nc"
@@ -127,7 +87,7 @@ def plot_comparison(lat_bounds, lon_bounds):
 
     plot_field(
         ax,
-        ds_month["tropomi"],
+        ds["tropomi"],
         cmap="Spectral_r",
         vmin=1750,
         vmax=1950,
@@ -151,7 +111,7 @@ def plot_comparison(lat_bounds, lon_bounds):
 
     plot_field(
         ax,
-        ds_month["diff_tropomi_prior"],
+        ds["diff_tropomi_prior"],
         cmap="RdBu_r",
         vmin=-40,
         vmax=40,
@@ -175,7 +135,7 @@ def plot_comparison(lat_bounds, lon_bounds):
 
     plot_field(
         ax,
-        ds_post_rounded["diff_tropomi_posterior"],
+        ds["diff_tropomi_posterior"],
         cmap="RdBu_r",
         vmin=-40,
         vmax=40,
@@ -193,56 +153,83 @@ def plot_comparison(lat_bounds, lon_bounds):
     )
 
 if __name__ == "__main__":
-    import sys
+    
+    satdat_dir = "./data_converted"
+    posterior_dir = "./data_converted_posterior"
+    visualization_dir = "./data_visualization"
+    posterior_viz_dir = "./data_visualization_posterior"
 
-    # run with srun --mem-per-cpu=5G python compare_gc_tropomi.py "YYYYMMDD"
-    # make sure sensi files are in data_converted
+    # Set up map bounds for plotting
+    lat_bounds = [-56.0, 76.0]
+    lon_bounds = [-170.0, 167.5]
 
-    month = sys.argv[1] # "YYYYMMDD" string format
-
-    data_dir_post = "./data_visualization_posterior"
-    data_dir_prior = f"./data_converted/sensi_{month}_{month}.nc"
-
-    df_month, df_post_month = match_observations(data_dir_prior, data_dir_post)
-
-    visualization_df = df_month.groupby(["lat", "lon"]).mean()
-    ds_month = visualization_df.to_xarray()   
-
-    # Round posterior to 2° x 2.5°
-    d = {var: df_post_month[var] for var in df_post_month.columns}
-    d["lat"] = np.round(df_post_month["lat"]/2.0)*2.0
-    d["lon"] = np.round(df_post_month["lon"]/2.5)*2.5
-    df_post_rounded = pd.DataFrame(d)
-    ds_post_rounded = df_post_rounded.groupby(["lat","lon"]).mean().to_xarray()
+    superobs_df = aggregate_data(satdat_dir, posterior_dir)
+    visualization_df = aggregate_data(visualization_dir, posterior_viz_dir)
+    n_obs = len(superobs_df["tropomi"])
 
     print(
-    "Bias in prior     :",
-    np.round(
-        np.average(
-            df_month["diff_tropomi_prior"], weights=df_month["observation_count"]
-        ),
-        2,
-    ),
-    "ppb",
+        f'Found {n_obs} super-observations in the domain, representing {np.sum(superobs_df["observation_count"]).round(0)} TROPOMI observations.'
     )
+    print(superobs_df.head())
 
+
+    # Print some error statistics
+    print(
+        "Bias in prior     :",
+        np.round(
+            np.average(
+                superobs_df["diff_tropomi_prior"], weights=superobs_df["observation_count"]
+            ),
+            2,
+        ),
+        "ppb",
+    )
     print(
         "Bias in posterior :",
         np.round(
             np.average(
-                df_post_rounded["diff_tropomi_posterior"],
-                # weights=df_month["observation_count"],
+                superobs_df["diff_tropomi_posterior"],
+                weights=superobs_df["observation_count"],
+            ),
+            2,
+        ),
+        "ppb",
+    )
+    print(
+        "RMSE prior        :",
+        np.round(
+            np.sqrt(
+                np.average(
+                    superobs_df["diff_tropomi_prior"] ** 2,
+                    weights=superobs_df["observation_count"],
+                )
+            ),
+            2,
+        ),
+        "ppb",
+    )
+    print(
+        "RMSE posterior    :",
+        np.round(
+            np.sqrt(
+                np.average(
+                    superobs_df["diff_tropomi_posterior"] ** 2,
+                    weights=superobs_df["observation_count"],
+                )
             ),
             2,
         ),
         "ppb",
     )
 
-    # Set up map bounds for plotting
-    lat_bounds = [-56.0, 76.0]
-    lon_bounds = [-170.0, 167.5]
+    # Simple averaging scheme to grid the XCH4 data at 2 x 2.5 resolution
+    df_copy = visualization_df.copy()  # save for later
+    visualization_df["lat"] = np.round(visualization_df["lat"]/2.0)*2.0
+    visualization_df["lon"] = np.round(visualization_df["lon"]/2.0)*2.0
+    visualization_df = visualization_df.groupby(["lat", "lon"]).mean()
+    ds = visualization_df.to_xarray()
 
     # Plot and save figures
-    plot_comparison(lat_bounds, lon_bounds)
+    plot_comparison(ds, lat_bounds, lon_bounds)
     print("Done plotting")
     
