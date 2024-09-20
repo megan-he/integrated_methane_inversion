e96facbc8accc19e85f505b2733ff7c3d78e78e9
megan-he <mhe@g.harvard.edu>
2024-09-20 11:39:43 -0400

    update for annual inversion

--- W.py
+++ W.py
@@ -2,6 +2,7 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from src.inversion_scripts.utils import filter_prior_files, get_mean_emissions
 
 def clusters_2d_to_1d(clusters, data, fill_value=0):
     '''
@@ -91,61 +92,90 @@ def source_attribution(w, xhat, shat=None, a=None):
     else:
         return xhat_red
 
+def analyze_OH(data_dir):
+    inversion_result = xr.load_dataset(f'{data_dir}/inversion/inversion_result.nc')
+    # Keep only OH elements for analysis
+    xhat_OH = inversion_result["xhat"][-2:].to_numpy()
+    S_post_OH = inversion_result["S_post"].isel(nvar1=slice(-2,None), nvar2=slice(-2,None)).to_numpy()
+    A_OH = inversion_result["A"].isel(nvar1=slice(-2,None), nvar2=slice(-2,None)).to_numpy()
+
+    print(S_post_OH)
+    print(A_OH)
+    print(np.trace(A_OH))
+
+
+def plot_correlation(w_matrix, kf=False):
+    '''Plot error correlation matrix given W'''
+
+    w = w_matrix.T
+    inversion_result = xr.load_dataset(f'{data_dir}/inversion/inversion_result.nc')
+    # Keep only emission elements for analysis
+    xhat_emissions = inversion_result["xhat"][:-2]
+    S_post = inversion_result["S_post"].isel(nvar1=slice(None,-2), nvar2=slice(None,-2))
+    A = inversion_result["A"].isel(nvar1=slice(None,-2), nvar2=slice(None,-2))
+
+    xhat_reduced, S_post_red, pearson, A_red = source_attribution(w, xhat_emissions, S_post, A)
+    cols = pearson.columns.tolist()
+
+    # Plot posterior error correlation matrix
+    fig = plt.figure(figsize=(19,15))
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(pearson.corr(), origin='lower', cmap='RdBu_r', vmin=-1, vmax=1)
+    ax.set_xticks(range(len(cols)))
+    ax.set_yticks(range(len(cols)))
+    ax.xaxis.set_ticks_position('bottom')
+    ax.set_xticklabels(cols, rotation=45, fontsize=18)
+    ax.set_yticklabels(cols, fontsize=18)
+    cbar = fig.colorbar(cax)
+    cbar.ax.tick_params(labelsize=20)
+    if kf:
+        plt.savefig(f'error_corr_{year}{i+5:02d}.png')
+    else:
+        plt.savefig(f'error_corr_{year}.png')
+
 if __name__ == "__main__":
 
-    import sys
-    make_W = sys.argv[1].lower() == 'true'
-    plot_correlation = sys.argv[2].lower() == 'true'
+    year = 2019
+    kalman_mode = False
+    start_date = "20190101"
+    end_date = "20200101"
 
-    data_dir = "/n/holyscratch01/jacob_lab/mhe/Global_2019_fast"
-    year = 2018
-    months = [i for i in range(5, 13)]
-    priors = [f'{data_dir}/prior_run/OutputDir/HEMCO_sa_diagnostics.{year}{m:02d}010000.nc'
-                 for m in months] # list of emissions for first day in each month
+    data_dir = f"/n/holyscratch01/jacob_lab/mhe/Global_{year}_annual"
+    months = [i for i in range(1, 13)]
+    emis_files = [f'{data_dir}/hemco_prior_emis/OutputDir/HEMCO_sa_diagnostics.{year}{m:02d}010000.nc'
+                for m in months] # list of emissions for first day in each month
+    prior_cache_path = f'{data_dir}/hemco_prior_emis/OutputDir/'
     sv_path = f"{data_dir}/StateVector.nc"
     sv = xr.open_dataset(sv_path)
     sv = sv.to_dataarray('StateVector')
 
-    if make_W:
-        # Save W matrix for each month
-        for i, prior in enumerate(priors):
-            ds = xr.load_dataset(prior)
-            
+    if kalman_mode:
+        for i, emis in enumerate(emis_files):
+            ds = xr.load_dataset(emis)
+            # Save W matrix for each month
             w = sectoral_matrix(sv, ds)
 
             # print(w.sum()*86400*31*1e-9) # Tg/yr
             print(f"Monthly total for {year}{i+5:02d}: {w.to_numpy().sum()*86400*31*1e-9} Tg")
             w.to_csv(f'{data_dir}/w_{year}{i+5:02d}.csv', index=False)
+            plot_correlation(w, kalman_mode)
 
-    if plot_correlation:
-        # Load W matrix
-        for i, prior in enumerate(priors):
-            w = pd.read_csv(f'{data_dir}/w_{year}{i+5:02d}.csv')
-            # anth_cols = ['Reservoirs', 'BiomassBurn', 'OtherAnth', 'Rice', 
-            #            'Wastewater', 'Landfills', 'Livestock', 'Coal', 'Gas', 'Oil']
-            # bio_cols = ['Wetlands']
-            # w['total'] = w.sum(axis=1)
-            # w['total_anth'] = w[anth_cols].sum(axis=1)
-            # w['total_bio'] = w[bio_cols].sum(axis=1)
-            w = w.T
-            inversion_result = xr.load_dataset(f'{data_dir}/kf_inversions/period{i+1}/inversion_result.nc')
-            # Keep only emission elements for analysis
-            xhat_emissions = inversion_result["xhat"][:-2]
-            S_post = inversion_result["S_post"].isel(nvar1=slice(None,-2), nvar2=slice(None,-2))
-            A = inversion_result["A"].isel(nvar1=slice(None,-2), nvar2=slice(None,-2))
+    else:
+        ds = get_mean_emissions(start_date, end_date, prior_cache_path)
+        ds.to_netcdf(f"{data_dir}/HEMCO_diagnostics.{year}.nc")
+        # Also check annual mean emissions
+        emissions_in_kg_per_s = ds["EmisCH4_Total"] * ds["AREA"]
+        total = emissions_in_kg_per_s.sum() * 86400 * 365 * 1e-9
+        print(f"{total.values:.2f} Tg/yr")
 
-            xhat_reduced, S_post_red, pearson, A_red = source_attribution(w, xhat_emissions, S_post, A)
-            cols = pearson.columns.tolist()
+        # Save annual mean W matrix
+        w = sectoral_matrix(sv, ds)
+        w_total = w.to_numpy().sum() * 86400 * 365 * 1e-9
+        print(f"{w_total:.2f} Tg/yr")
+        w.to_csv(f'{data_dir}/w_{year}_annual.csv', index=False)
 
-            # Plot posterior error correlation matrix
-            fig = plt.figure(figsize=(19,15))
-            ax = fig.add_subplot(111)
-            cax = ax.matshow(pearson.corr(), origin='lower', cmap='RdBu_r', vmin=-1, vmax=1)
-            ax.set_xticks(range(len(cols)))
-            ax.set_yticks(range(len(cols)))
-            ax.xaxis.set_ticks_position('bottom')
-            ax.set_xticklabels(cols, rotation=45, fontsize=18)
-            ax.set_yticklabels(cols, fontsize=18)
-            cbar = fig.colorbar(cax)
-            cbar.ax.tick_params(labelsize=20)
-            plt.savefig(f'error_corr_{year}{i+5:02d}.png')
+        # Plot
+        plot_correlation(w, kalman_mode)
+
+        # Print OH statistics
+        analyze_OH(data_dir)
